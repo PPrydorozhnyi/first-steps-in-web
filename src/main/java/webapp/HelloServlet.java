b39bd5b5650f29177942b2e32d3f2ef68f542f70
Peter Pridorozhny <Peter35128@gmail.com>
2017-08-23 16:42:32 +0300

    Java Servlet session

--- src/main/java/webapp/HelloServlet.java
+++ src/main/java/webapp/HelloServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -17,6 +18,16 @@ public class HelloServlet extends HttpServlet {
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         resp.getWriter().append("Hello World!" + req.getMethod());
+
+        //HttpSession
+        // remember potential user
+        HttpSession session = req.getSession();
+
+        if (session.getValue("name") == null) {
+            session.putValue("name", "Hello world session");
+            resp.getWriter().append(" No session");
+        } else
+            resp.getWriter().append((String)session.getValue("name"));
 
     }
 }
